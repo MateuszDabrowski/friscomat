a2c96b28e047316917a410914557c7a9a78cba2b
Mateusz Dąbrowski <github@mateuszdabrowski.pl>
2020-04-20 13:52:24 +0200

    Refactored the code and added logic for getting available timeframes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-continue */
 const puppeteer = require('puppeteer');
-const config = require('dotenv/config.js');
+require('dotenv/config.js');
 
-const headless = process.env.NODE_ENV === 'development' ? { headless: false } : null;
+const headless = process.env.NODE_ENV === 'development' ? { headless: false } : undefined;
 
 (async () => {
     const browser = await puppeteer.launch(headless);
@@ -11,53 +11,54 @@ const headless = process.env.NODE_ENV === 'development' ? { headless: false } : 
 
     // Go to Frisco page
     await page.goto('https://www.frisco.pl', { waitUntil: 'networkidle2' });
+    console.log('On Frisco page');
 
-    // Close main popup if opened
-    if (await page.$('.image-popups')) {
-        await page.click('.image-popups > div > div > .close');
+    // Close popups if opened
+    if (await page.$('a.close')) {
+        await page.$$eval('a.close', (closeButtons) => closeButtons.map((button) => button.click()));
     }
+
     // Close cookie popup if opened
     if (await page.$('.mini-notification_close')) {
         await page.click('.mini-notification_close');
     }
 
-    // Open login menu
-    await page.click('.login > div > .cta');
+    console.log('Popups closed');
+    // Check login status
+    const [notLogged] = await page.$x("//a[contains(., 'Zaloguj się')]");
+    if (notLogged) {
+        console.log('Logging in');
+        // Open login menu
+        await notLogged.click();
 
-    for (let i = 0; i < 10; i += 1) {
-        await page.waitFor(i * 1000);
-        // Pass in login details
-        try {
-            await page.waitForSelector('.form-input_email > div > input');
-            await page.type('.popup_login-inputs-wrapper > .form-input_email > div > input', process.env.FRISCO_LOGIN);
-            await page.type(
-                '.popup_login-inputs-wrapper > .form-input_password > div > input',
-                process.env.FRISCO_PASSWORD
-            );
-        } catch (error) {
-            continue;
-        }
+        for (let i = 0; i < 3; i += 1) {
+            await page.waitFor(i * 1000);
+            // Pass in login details
+            try {
+                await page.type('input[name="username"]', process.env.FRISCO_LOGIN);
+                await page.type('input[name="password"]', process.env.FRISCO_PASSWORD);
+            } catch (error) {
+                continue;
+            }
 
-        // Log in to Frisco
-        await page.click('.popup_box-buttons > input');
+            // Log in to Frisco
+            await page.click('input.login[type="submit"]');
 
-        // Check if login was successful
-        await page.waitForSelector('.header_user-menu');
-        const login = await page.$eval('.header_user-menu', (element) => element.textContent);
-        if (!login.includes('Zaloguj')) {
-            break;
+            // Check if login was successful
+            await page.waitForSelector('.header_user-menu');
+            if (!notLogged) {
+                break;
+            }
         }
     }
+    console.log('Logged in');
 
     // Check reservation date
-    for (let i = 0; i < 10; i += 1) {
+    let deliveryDate = '';
+    for (let i = 0; i < 3; i += 1) {
         await page.waitFor(i * 1000);
         try {
-            const deliveryDate = await page.$eval(
-                '.header_delivery-inner > div > .date',
-                (element) => element.textContent
-            );
-            console.log(deliveryDate);
+            deliveryDate = await page.$eval('div.date', (element) => element.textContent);
             if (deliveryDate) {
                 break;
             }
@@ -65,12 +66,13 @@ const headless = process.env.NODE_ENV === 'development' ? { headless: false } : 
             continue;
         }
     }
+    console.log(deliveryDate);
 
     // TODO: Logic for assessing interesting delivery date and looping if it is not interesting
 
     // Open reservation panel
     for (let i = 0; i < 10; i += 1) {
-        await page.click('.header_delivery-inner > div > .subline');
+        await page.click('div.date');
         await page.waitFor(i * 1000);
         try {
             const reservations = await page.$eval(
@@ -89,9 +91,16 @@ const headless = process.env.NODE_ENV === 'development' ? { headless: false } : 
     await page.waitForSelector('.reservation-selector_tabs-content');
     await page.click('.reservation-selector_tabs-content > .button');
 
-    // TODO: Logic for choosing the interesting timeframe for delivery
+    // Get all available hours
+    await page.waitForSelector('span.available');
+    const deliveryHours = await page.$$eval('span.available > .hours', (timeslots) =>
+        timeslots.map((timeframe) => timeframe.textContent)
+    );
+    console.log(deliveryHours);
+
+    console.log('End');
 
     // await browser.close();
 })();
 
-// TODO: Notification on sucess
+// TODO: Slack integration
